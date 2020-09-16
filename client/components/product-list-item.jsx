506187e114fb2e12d3d11080f842e13d9d6b6431
Joseph Nguyen <josephsportycs@gmail.com>
2020-09-16 14:43:11 -0700

    Changes made 3.

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 function ProductListItem(props) {
   return (
-    <div className="product-container">
-      {props.products}
+    <div key={props.item.productId} className="product">
+      <img src={props.item.image} />
+      <h2>{props.item.name}</h2>
+      <h4>{props.item.price}</h4>
+      <p>{props.item.shortDescription}</p>
     </div>
   );
 
--- client/components/product-list.jsx
+++ client/components/product-list.jsx
@@ -22,17 +22,12 @@ class ProductList extends React.Component {
   render() {
     const items = this.state.products.map(item => {
       return (
-        <div key={item.productId} className="product">
-          <img src={item.image} />
-          <h2>{item.name}</h2>
-          <h4>{item.price}</h4>
-          <p>{item.shortDescription}</p>
-        </div>
+        <ProductListItem key={item.productId} item={item}/>
       );
     });
     return (
-      <div>
-        <ProductListItem products={items}/>
+      <div className="product-container">
+        {items}
       </div>
     );
   }

00a5a9614d2acf3627f2b3e33f78ea646d8b44df
Joseph Nguyen <josephsportycs@gmail.com>
2020-09-16 15:13:18 -0700

    Completed view details product backend.

--- server/index.js
+++ server/index.js
@@ -29,6 +29,27 @@ app.get('/api/products', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.get('/api/products/:productId', (req, res, next) => {
+  const productId = parseInt(req.params.productId, 10);
+  const sql = `
+  select *
+  from "products"
+  where "productId" = $1
+  `;
+  const params = [productId];
+  db.query(sql, params)
+    .then(result => {
+      const product = result.rows[0];
+      if (!product) {
+        res.status(500).json({
+          error: `Can't find product with productId ${productId}`
+        });
+      }
+      res.json(product);
+    })
+    .catch(err => next(err));
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
